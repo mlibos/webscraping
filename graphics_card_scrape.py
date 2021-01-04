457aac19d74eb7715be77901a5c3ceab547a0f2b
Munir Libos <munir.libos@gmail.com>
2021-01-03 19:23:17 -0600

    Update graphics_card_scrape.py

--- graphics_card_scrape.py
+++ graphics_card_scrape.py
@@ -5,7 +5,7 @@ import pandas
 
 filename = 'products.csv'
 f = open(filename,'w')
-headers = 'brand, product_name, shipping\n'
+headers = 'brand, product_name, price, shipping\n'
 f.write(headers)
 url = "https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38"
 uclient = ureq('https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38')
@@ -15,15 +15,13 @@ page_soup = soup(page_html,'html.parser')
 containers = page_soup.findAll('div',{'class':'item-container'})
 for container in containers:
 	brand = container.div.div.a.img['title']
-
 	title = container.findAll('a',{'class':'item-title'})[0].text
-	
+	price_class = container.findAll('ul',{'class':'price'})[0]
+	price = price_class.findAll('li',{'class':'price-current'})[0].strong.text
 	shipping = container.findAll('li',{'class':'price-ship'})[0].text.strip()
-	print('brand '+ brand)
-	print('title ' + title)
-	print('shipping ' + shipping)
-	f.write(brand+',' + title.replace(',','|') + ',' + shipping + '\n')
+	f.write(brand+',' + title.replace(',','|') + ',' + price + ',' + shipping + '\n')
 f.close()
 
 df_product = pandas.read_csv(filename)
 print(df_product)
+
